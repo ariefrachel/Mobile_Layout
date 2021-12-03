077d3497f98b4a9fe6a4cc041c20f671e1a8f616
riefrachman <57061770+riefrachman@users.noreply.github.com>
2021-12-04 00:04:57 +0700

    first commit

---
+++ app/src/main/java/com/arief/praktek3/GridLayout.kt
@@ -0,0 +1,11 @@
+package com.arief.praktek3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GridLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_grid_layout)
+    }
+}
